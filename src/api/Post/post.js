6ee9a2ba72f9a418441cea40a57faca27d41175c
philjae <philjae@gmail.com>
2020-07-19 10:39:00 +0900

    Hands-on-Demo01 - Relational Data

--- src/api/Post/post.js
+++ src/api/Post/post.js
@@ -6,20 +6,12 @@ export default {
       let resultAuthor = null;
 
       try {
+        //console.log(_.authorId);
 
-        //console.dir(_);
+        resultAuthor = users.find(user => user.id == _.authorId);
 
-        switch (_.id) {
-          case 1:
-          case 2:
-            resultAuthor = users[0]
-            break;
-          case 3:
-          case 4:
-          case 5:
-            resultAuthor = users[1]
-            break;
-        }
+        console.log(resultAuthor);
+
       } catch (e) {
         console.log("Post.author resolver exception: " + e);
       }
